b0d92c5063fb55b664b34abf93d3c3031f5b2a20
J4ndro <jandrohinarejos@gmail.com>
2025-02-21 09:57:44 +0100

    Añadido lista

--- listado.js
+++ listado.js
@@ -2,6 +2,7 @@
 
 
 function Main() {
+  let lista = []
   let datos = []
   let selected = null
   let comercioSelected = null
@@ -9,14 +10,14 @@ function Main() {
   return {
     model:{
       comercios: [
-        {id: 1, name: "Quesomentero"},
-        {id: 2, name: "Frutos Secos Chelo y Rocio"},
-        {id: 3, name: "Pescaderia Toni Cuartero"},
-        {id: 4, name: "Frutas y Verduras Mari"},
-        {id: 5, name: "Pescaderia Sylvia"},
-        {id: 6, name: "Vinos y Embutidos Santiago Manrique"},
-        {id: 7, name: "Menjar preparat la cuina del Peix"},
-        {id: 8, name: "Gelats i Torrons Maria Coloma"}
+        {id: 1, name: "Quesomentero", logo:"images/imagenes_comercios/logo-mentero.jpg", banner:"images/imagenes_comercios/escaparat-mentero.jpg"},
+        {id: 2, name: "Frutos Secos Chelo y Rocio", logo:"images/imagenes_comercios/logo-fruitssecs.jpg", banner:"images/imagenes_comercios/fruitssecs_DSC4553.jpg"},
+        {id: 3, name: "Pescaderia Toni Cuartero",logo:"images/imagenes_comercios/logo-peixcater.jpg", banner:"images/imagenes_comercios/peix2_DSC3532.jpg"},
+        {id: 4, name: "Frutas y Verduras Mari",logo:"images/imagenes_comercios/logo-fruitera.jpg", banner:"images/imagenes_comercios/fruita_DSC4109.jpg"},
+        {id: 5, name: "Pescaderia Sylvia",logo:"images/imagenes_comercios/logo-peixactera.jpg", banner:"images/imagenes_comercios/peix_DSC3532.jpg"},
+        {id: 6, name: "Vinos y Embutidos Santiago Manrique",logo:"images/imagenes_comercios/logo-manrique.jpg", banner:"images/imagenes_comercios/escaparat-xarcutera-1-scaled.jpg"},
+        {id: 7, name: "Menjar preparat la cuina del Peix",logo:"images/imagenes_comercios/logo-fruitera.jpg", banner:"images/imagenes_comercios/escaparat-maria-coloma-1-scaled.jpg"},
+        {id: 8, name: "Gelats i Torrons Maria Coloma",logo:"images/imagenes_comercios/logo-maria-coloma.jpg", banner:"images/imagenes_comercios/escaparat-maria-coloma-1-scaled.jpg"}
       ]
       
     },
@@ -27,13 +28,20 @@ function Main() {
       : !comercioSelected
       ?[m("div",{style:{maxWidth:"1140px"}}, 
         m("h1",{style:{textAlign:"center",padding:"15px 0"}}, "Nuestros Comercios"),
-        m("div", {style:{display:"flex",width:"100%",flexWrap:"wrap", justifyContent:"center", gap: "6px"}},
-          console.log(datos),
+        m("div", {style:{display:"flex",width:"100%",flexWrap:"wrap", justifyContent:"center", gap: "20px"}},
           this.state.model.comercios.map(comercio=>[
             
-            m("div", {style:{flex:"1 0 33%",height:"300px",justifyContent:"center",alignItems:"center",display:"flex", flexDirection:"column", borderRadius:"33px", backgroundColor:"#f2f2f2"}}, [
-              m("h2", comercio.name),
-              m("button", {onclick:()=>{comercioSelected=comercio;console.log(comercioSelected)}}, "Seleccionar"),
+            m("div", {style:{flex:"1 0 33%",height:"350px",justifyContent:"center",alignItems:"center",display:"flex", flexDirection:"column", borderRadius:"33px", backgroundColor:"#f2f2f2"}}, [
+              m("div",{style:{height:"60%", width:"100%",display:"flex", justifyContent:"center", alignItems:"center",backgroundImage:`url(${comercio.banner|| "images/imagenes_comercios/escaparat-mentero.jpg"})` ,backgroundSize:"cover",backgroundPosition:"center",borderTopLeftRadius:"33px",borderTopRightRadius:"33px"}},),
+              m("div",{style:{height:"40%", width:"100%",display:"flex", justifyContent:"space-around", alignItems:"center",backgroundColor:"#00673A",borderBottomLeftRadius:"33px",borderBottomRightRadius:"33px"}},
+                [
+                  m("div",{style:{borderRadius:"99px",backgroundColor:"white",display:"flex"}},
+                  m("img",{src:comercio.logo || "./images/imagenes_comercios/logo-mentero.jpg",style:{padding:"5px", width:"100px",height:"100px", borderRadius:"99px"}}),
+                  ),
+                  m("h2",{style:{width:"70%",color:"white"}}, comercio.name)
+
+                ]
+              ),
             ])
 
           ])
@@ -46,7 +54,8 @@ function Main() {
           m(ListadoProductos)
         ),
         ]
-        )
+        ),
+        m("div")
       ]
       : [
         m("div", {style:{display:"flex",width:"100%",flexWrap:"wrap", justifyContent:"center", gap: "6px", alignItems:"center"}},
@@ -293,11 +302,37 @@ function Main() {
                     m("h2",{style:{marginBottom:"16px", marginLeft:"12px", fontSize:"1.25em", lineHeight:"1.25"}}, subcategoria2.name),
                     m("div",{style:{display:"flex",flexWrap:"wrap",justifyContent:"center",flexDirection:"row",padding:"100px 0",borderRadius:"33px", gap:"10px"}}, subcategoria2.products.map((product,index)=> [                    
                         m("div", {style:{borderRadius:"33px",flex:"0 1 200px",height:"340px", backgroundColor:"#f2f2f2",display:"flex",flexDirection:"column",justifyContent:"space-between",alignItems:"center", padding:"10px 0px"}},
-                          m("img",{src:"5102174.png",width:"100px",height:"100px",style:{marginTop:"50px"}}), 
+  
+                           m("img",{src: 
+                            subcategoria.name && subcategoria.name.toLowerCase().includes("pescado")  
+                            ? `images/pescaderia/image${Math.floor(Math.random() * (119 - 2 + 1)) + 2}.jpg`
+                            : subcategoria.name && subcategoria.name.toLowerCase().includes("vino") 
+                            ? `images/pescaderia/image${Math.floor(Math.random() * (119 - 2 + 1)) + 2}.jpg`
+                            :"5102174.png",width:"200px",height:"200px",style:{marginTop:"50px"}}),
+   
                           m("div",{style:{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}},
                           m("p",{style:{textAlign:"center",padding:"0 5px"}}, `${product.name}`),
-                          m("br"), 
-                          m("span",{style:{fontWeight:"bold",margin:"0 0 15px 0"}}, `${product.price}€`)
+                          m("span",{
+                            onclick: () => {
+                              if (!lista.categorias) {
+                                // Si lista.categorias no existe, lo creamos con la nueva categoría y producto
+                                lista.categorias = [{ name: subcategoria.name.toLowerCase(), products: [product] }];
+                              } else {
+                                // Buscamos la categoría dentro del array
+                                let categoria = lista.categorias.find(cat => cat.name === subcategoria.name.toLowerCase());
+                            
+                                if (categoria) {
+                                  // Si la categoría existe, añadimos el producto a la lista de productos
+                                  categoria.products.push(product);
+                                } else {
+                                  // Si no existe la categoría, la agregamos junto con el producto
+                                  lista.categorias.push({ name: subcategoria.name.toLowerCase(), products: [product] });
+                                }
+                              }
+                            
+                              console.log(lista);
+                            },
+                            style:{fontWeight:"bold",margin:"0 0 15px 0",cursor:"pointer"}}, `Añadir a la lista`)
                           )
                         ) 
                       
